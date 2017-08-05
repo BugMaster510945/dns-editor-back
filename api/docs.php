2a4fd3da2a140afa3f85b11266ca5a936135755b
Frédéric Planchon <github@planchon.org>
2017-08-05 10:22:18 +0200

    Suppression du finder x100 sur les perf

--- api/docs.php
+++ api/docs.php
@@ -64,63 +64,37 @@ while( count($stack) > 0 )
 		closedir($dh);
     }
 }
-var_dump($files);
 
-exit;
-
-
-$need_extract = true;
-
+$swagger = null;
 if( $doCache )
 {
-	$data = null;
-	if( is_readable(CACHE_DIR.'/docs.json') )
-		$data = unserialize(file_get_contents(CACHE_DIR.'/docs.json'));
-	if( !is_array($data) ) 
-		$data = array();
-
-	if( array_key_exists('files', $data) )
-	{
-		$last_modif = 0;
-		foreach ($data['files'] as $file)
-		{
-			$last_modif = max($last_modif, @filemtime($file));
-		}
-		$need_extract = $data['filemtime'] < $last_modif;
-	}
-	
+	if( is_readable(CACHE_DIR.'/docs.json') && ($last_modif <= @filemtime(CACHE_DIR.'/docs.json')) )
+		$swagger = unserialize(file_get_contents(CACHE_DIR.'/docs.json'));
+	if( ! $swagger instanceof Swagger\Annotations\Swagger ) 
+		$swagger = null;
 }
 
-if( $need_extract )
+if( is_null($swagger) )
 {
-	$data = array( 'files' => [] );
 	$analyser = new \Swagger\StaticAnalyser();
 	$analysis = new \Swagger\Analysis();
 	$processors = \Swagger\Analysis::processors();
 
 	// Crawl directory and parse all files
-	$finder = \Swagger\Util::finder(PATH_BASE, array('vendor', 'old', 'dns-editor-front', 'named-zone', 'netdns2'));
-	foreach ($finder as $file)
+	foreach ($files as $file)
 	{
-		$f = $file->getPathname();
-		$data['files'][] = $f;
-		$analysis->addAnalysis($analyser->fromFile($f));
+		$analysis->addAnalysis($analyser->fromFile($file));
 	}
 	// Post processing
 	$analysis->process($processors);
 	// Validation (Generate notices & warnings)
 	$analysis->validate();
 	$swagger = $analysis->swagger;
+
 	if( $doCache )
 	{
-		$data['filemtime'] = time();
-		$data['swagger'] = $swagger;
-		file_put_contents(CACHE_DIR.'/docs.json', serialize($data));
+		@file_put_contents(CACHE_DIR.'/docs.json', serialize($swagger));
 	}
-}
-else
-{
-	$swagger = $data['swagger'];
 }
 
 sendJSON($swagger);
